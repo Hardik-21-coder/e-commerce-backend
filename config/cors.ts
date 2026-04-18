9192759c46d51119921893f02cea83f8fb75a2bc
Hardik-21-coder <hardikdegama21@gmail.com>
2026-04-18 23:21:52 +0530

    add cors and add neondatabase

--- config/cors.ts
+++ config/cors.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@adonisjs/cors'
 
 export default defineConfig({
   enabled: true,
-  origin: ['https://e-commerce-two-kappa-11.vercel.app', 'http://localhost:5173'],
+  origin: ['https://e-commerce-two-kappa-11.vercel.app', 'http://localhost:3000'],
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
   headers: true,
   exposeHeaders: [],
